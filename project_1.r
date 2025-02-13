735a88c9302f21dad44d205bff3b5b7ac58f0306
KidQuant <38800128+KidQuant@users.noreply.github.com>
2025-02-13 13:22:39 -0500

    updates to project report, tidyquant code and project_1.r code

--- project_1.r
+++ project_1.r
@@ -3,51 +3,129 @@ library("fBasics")
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-symbols <- c("ADBE", "AMD", "MSFT", "QCOM", "EBAY", "ORCL")
+symbols <- c("ADBE", "AMD", "MSFT", "QCOM", "CSCO", "INTU")
 getSymbols(symbols)
+#########################
+# MSFT
+##########################
 
-msft_returns <- dailyReturn(MSFT$MSFT.Close)
-msft_returns <- msft_returns * 100
+
+msft_returns <- dailyReturn(MSFT$MSFT.Close) * 100
 basicStats(msft_returns)
-log_msft_returns <- log(msft_returns + 1)
-log_msft_returns <- log_msft_returns * 100
-basicStats(log_msft_returns)
 
-amd_returns <- dailyReturn(AMD$AMD.Close)
-amd_returns <- amd_returns * 100
+log_msft_returns <- log(msft_returns + 1)
+
+basicStats(log_msft_returns)
+normalTest(log_msft_returns,method="jb")
+
+#########################
+# AMD
+##########################
+
+amd_returns <- dailyReturn(AMD$AMD.Close) *100
 basicStats(amd_returns)
 log_amd_returns <- log(amd_returns + 1)
-log_amd_returns <- log_amd_returns * 100
+
 basicStats(log_amd_returns)
 
-adbe_returns <- dailyReturn(ADBE$ADBE.Close)
-adbe_returns <- adbe_returns * 100
+normalTest(log_amd_returns,method="jb")
+
+#########################
+# ADBE
+##########################
+
+adbe_returns <- dailyReturn(ADBE$ADBE.Close) *100
 basicStats(adbe_returns)
 log_adbe_returns <- log(adbe_returns + 1)
-log_adbe_returns <- log_adbe_returns * 100
+
 basicStats(log_adbe_returns)
 
+normalTest(log_adbe_returns,method="jb")
 
-qcom_returns <- dailyReturn(QCOM$QCOM.Close)
-qcom_returns <- qcom_returns * 100
+#########################
+# QCOM
+##########################
+
+qcom_returns <- dailyReturn(QCOM$QCOM.Close) *100
 basicStats(qcom_returns)
 log_qcom_returns <- log(qcom_returns + 1)
-log_qcom_returns <- log_qcom_returns * 100
+
 basicStats(log_qcom_returns)
-
-orcl_returns <- dailyReturn(ORCL$ORCL.Close)
-log_orcl_returns <- log(orcl_returns + 1)
-basicStats(log_orcl_returns)
-
-
-typeof(symbol)
-head(MSFT)
-
-normalTest(log_msft_returns,method="jb")
-normalTest(log_amd_returns,method="jb")
-normalTest(log_adbe_returns,method="jb")
 normalTest(log_qcom_returns,method="jb")
 
+#########################
+# CSCO
+##########################
+
+csco_returns <- dailyReturn(CSCO$CSCO.Close) * 100
+basicStats(csco_returns)
+
+log_csco_returns <- log(csco_returns + 1)
+
+basicStats(log_csco_returns)
+
+normalTest(log_csco_returns,method="jb")
+
+#########################
+# INTU
+##########################
+
+intu_returns <- dailyReturn(INTU$INTU.Close) *100
+basicStats(intu_returns)
+log_intu_returns <- log(intu_returns + 1)
+
+basicStats(log_intu_returns)
 
 
 
+
+
+
+
+
+
+par(mfrow=c(2,3))  # Set up the plotting area to have 2 rows and 2 columns
+
+acf(log_msft_returns, main="MSFT", lag.max=20)
+acf(log_amd_returns, main="AMD ", lag.max=20)
+acf(log_adbe_returns, main="ADBE", lag.max=20)
+acf(log_qcom_returns, main="QCOM", lag.max=20)
+acf(log_csco_returns, main="CSCO", lag.max=20)
+acf(log_intu_returns, main="INTU", lag.max=20)
+
+par(mfrow=c(1,1))  # Reset the plotting area to default
+
+
+# Save the plot to the specified directory
+plot_path <- "C:/Users/drebi/dev/FA542Project/Project Report #1/plots/acf_plots.png"
+png(filename=plot_path)
+
+par(mfrow=c(1,4))  # Set up the plotting area to have 1 row and 4 columns
+
+acf(log_msft_returns, main="ACF of MSFT Log Returns", lag.max=20)
+acf(log_amd_returns, main="ACF of AMD Log Returns", lag.max=20)
+acf(log_adbe_returns, main="ACF of ADBE Log Returns", lag.max=20)
+acf(log_qcom_returns, main="ACF of QCOM Log Returns", lag.max=20)
+
+par(mfrow=c(1,1))  # Reset the plotting area to default
+
+Box.test(log_amd_returns,lag=17,type='Ljung')
+Box.test(log_adbe_returns,lag=6,type='Ljung')
+Box.test(log_adbe_returns,lag=12,type='Ljung')
+Box.test(log_qcom_returns,lag=19,type='Ljung')
+Box.test(log_qcom_returns,lag=20,type='Ljung')
+Box.test(log_csco_returns,lag=14,type='Ljung')
+Box.test(log_csco_returns,lag=9,type='Ljung')
+Box.test(log_intu_returns,lag=18,type='Ljung')
+
+
+par(mfrow=c(2,3))  # Set up the plotting area to have 2 rows and 2 columns
+
+pacf(log_msft_returns, main="MSFT", lag.max=20)
+pacf(log_amd_returns, main="AMD ", lag.max=20)
+pacf(log_adbe_returns, main="ADBE", lag.max=20)
+pacf(log_qcom_returns, main="QCOM", lag.max=20)
+pacf(log_csco_returns, main="CSCO", lag.max=20)
+pacf(log_intu_returns, main="INTU", lag.max=20)
+
+par(mfrow=c(1,1))
