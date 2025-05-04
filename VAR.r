875390f730ec65304cb251d99717465eff7fa53a
KidQuant <38800128+KidQuant@users.noreply.github.com>
2025-05-04 04:09:57 -0400

    updates

--- VAR.r
+++ VAR.r
@@ -6,21 +6,24 @@ library(TSA)
 library(ggplot2)
 library(MTS)
 
-symbols <- c("NVDA", "QCOM")
+symbols <- c("AMD", "QCOM")
 getSymbols(symbols, src = "yahoo", from = "2007-01-01", to = "2025-02-10")
 
-nvda_close <- na.omit(Cl(NVDA))
-qcom_close <- na.omit(Cl(QCOM))
 
-stocks <- cbind(nvda_close, qcom_close)
+amd_returns <- monthlyReturn(AMD$AMD.Close) 
+log_amd_returns <- log(amd_returns + 1) * 252
 
-log_returns_nvda <- diff(log(nvda_close))[-1] * 252
-log_returns_qcom <- diff(log(qcom_close))[-1] * 252
+qcom_returns <- monthlyReturn(QCOM$QCOM.Close) 
+log_qcom_returns <- log(qcom_returns + 1) * 252
 
-combined_log_returns <- cbind(log_returns_nvda, log_returns_qcom)
+combined_log_returns <- cbind(log_amd_returns, log_qcom_returns)
 MTSplot(combined_log_returns)
 ccm(combined_log_returns)
+VARorder(log_qcom_returns)
+VARorder(log_amd_returns)
 
-
-
-ccf_result <- ccf(as.numeric(combined_log_returns[,1]), as.numeric(combined_log_returns[,2]), lag.max = 12, plot=TRUE)
+VARselect(combined_log_returns, lag.max = 12, type = "const")
+var_model <- VAR(combined_log_returns, p = 1, type = "const")
+var_model
+# Perform the multivariate Ljung-Box test
+mq(combined_log_returns, lag = 12)
